6e0f3ec09393ee45ff8ee90a5ab07e0b40d43068
Howard Lo <he01350446@gmail.com>
2016-11-25 21:37:03 +0800

    Complete initial carousel and visibility transition between landing page and search result when search bar is focused/blur

--- linggle/static/js/itri/main.js
+++ linggle/static/js/itri/main.js
@@ -1,1 +1,31 @@
-alert("Hi");
+var SearchBar = {
+
+  registerEvents: function() {
+    var searchBar = $('.linggle #search-bar');
+
+    searchBar.focus(this.handleOnFocus);
+    searchBar.blur(this.handleOnBlur);
+  },
+
+  handleOnFocus: function() {
+    var landingPage = $('.linggle.landing');
+    var searchResultPage = $('.linggle.search-result');
+
+    // toggle visibility
+    landingPage.hide();
+    searchResultPage.fadeIn(200);
+  },
+
+  handleOnBlur: function() {
+    var landingPage = $('.linggle.landing');
+    var searchResultPage = $('.linggle.search-result');
+
+    // toggle visibility
+    landingPage.fadeIn(200);
+    searchResultPage.hide();
+  }
+}
+
+$(document).ready(function() {
+  SearchBar.registerEvents();
+});
