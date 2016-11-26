7393058b0e678ae36cfe09795547f8809529911d
JJC <jjc@nlplab.cc>
2016-11-26 18:52:48 +0800

    use mini-linggle temporarily

--- linggle/index/views.py
+++ linggle/index/views.py
@@ -3,14 +3,23 @@ from django.http import HttpResponse
 import requests
 import urllib
 
+API_URL = 'http://ironman.nlpweb.org:9487/?search={}'
+
 
 def index(request):
     return render(request, 'index/index.html')
 
+
 def itri(request):
     return render(request, 'itri/index.html')
 
+
+# def linggleit(request, query):
+#     url = 'http://linggle.com/query/{}'.format(urllib.quote(query, safe=''))
+#     r = requests.get(url)
+#     return HttpResponse(content=r.text, status=r.status_code)
+
+
 def linggleit(request, query):
-    url = 'http://linggle.com/query/{}'.format(urllib.quote(query, safe=''))
-    r = requests.get(url)
+    r = requests.get(API_URL.format(query))
     return HttpResponse(content=r.text, status=r.status_code)
